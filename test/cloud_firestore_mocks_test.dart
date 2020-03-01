6ac3c12ccceb4369a2d35d7d4e80ccfff4823b85
Anh Tuan Nguyen <atn832@gmail.com>
2020-03-01 12:03:41 +0900

    move MockQuery related tests to its own file

--- test/cloud_firestore_mocks_test.dart
+++ test/cloud_firestore_mocks_test.dart
@@ -2,6 +2,9 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:cloud_firestore_mocks/cloud_firestore_mocks.dart';
 import 'package:test/test.dart';
 
+import 'document_snapshot_matcher.dart';
+import 'query_snapshot_matcher.dart';
+
 const expectedDumpAfterSetData = """{
   "users": {
     "abc": {
@@ -121,38 +124,6 @@ void main() {
             'name': 'Bob',
           })));
     });
-    test('Chained where queries return the correct snapshots', () async {
-      final instance = MockFirestoreInstance();
-      final bookmarks = await instance
-          .collection('users')
-          .document(uid)
-          .collection('bookmarks');
-      await bookmarks.add({
-        'hidden': false,
-      });
-      await bookmarks.add({
-        'tag': 'mostrecent',
-        'hidden': false,
-      });
-      await bookmarks.add({
-        'hidden': false,
-      });
-      await bookmarks.add({
-        'tag': 'mostrecent',
-        'hidden': true,
-      });
-      instance
-          .collection('users')
-          .document(uid)
-          .collection('bookmarks')
-          .where('hidden', isEqualTo: false)
-          .where('tag', isEqualTo: 'mostrecent')
-          .snapshots()
-          .listen(expectAsync1((QuerySnapshot snapshot) {
-        expect(snapshot.documents.length, equals(1));
-        expect(snapshot.documents.first.data['tag'], equals('mostrecent'));
-      }));
-    });
     test(
         'Snapshots sets exists property to false if the document does not exist',
         () async {
@@ -218,161 +189,8 @@ void main() {
             })
           ])));
     });
-    test('Where(field, isGreaterThan: ...)', () async {
-      final instance = MockFirestoreInstance();
-      final now = DateTime.now();
-      await instance.collection('messages').add({
-        'content': 'hello!',
-        'uid': uid,
-        'timestamp': now,
-      });
-      // Test that there is one result.
-      expect(
-          instance
-              .collection('messages')
-              .where('timestamp',
-                  isGreaterThan: now.subtract(Duration(seconds: 1)))
-              .snapshots(),
-          emits(QuerySnapshotMatcher([
-            DocumentSnapshotMatcher('z', {
-              'content': 'hello!',
-              'uid': uid,
-              'timestamp': Timestamp.fromDate(now),
-            })
-          ])));
-      // Filter out everything and check that there is no result.
-      expect(
-          instance
-              .collection('messages')
-              .where('timestamp', isGreaterThan: now.add(Duration(seconds: 1)))
-              .snapshots(),
-          emits(QuerySnapshotMatcher([])));
-    });
-  });
-  test('isLessThanOrEqualTo', () async {
-    final instance = MockFirestoreInstance();
-    final now = DateTime.now();
-    final before = now.subtract(Duration(seconds: 1));
-    final after = now.add(Duration(seconds: 1));
-    await instance.collection('messages').add({
-      'content': 'before',
-      'timestamp': before,
-    });
-    await instance.collection('messages').add({
-      'content': 'during',
-      'timestamp': now,
-    });
-    await instance.collection('messages').add({
-      'content': 'after',
-      'timestamp': after,
-    });
-    // Test filtering.
-    expect(
-        instance
-            .collection('messages')
-            .where('timestamp', isLessThanOrEqualTo: now)
-            .snapshots(),
-        emits(QuerySnapshotMatcher([
-          DocumentSnapshotMatcher('z', {
-            'content': 'before',
-            'timestamp': Timestamp.fromDate(before),
-          }),
-          DocumentSnapshotMatcher('zz', {
-            'content': 'during',
-            'timestamp': Timestamp.fromDate(now),
-          }),
-        ])));
-    expect(
-        instance
-            .collection('messages')
-            .where('timestamp',
-                isLessThanOrEqualTo: now.subtract(Duration(seconds: 2)))
-            .snapshots(),
-        emits(QuerySnapshotMatcher([])));
-    expect(
-        instance
-            .collection('messages')
-            .where('timestamp', isLessThan: now)
-            .snapshots(),
-        emits(QuerySnapshotMatcher([
-          DocumentSnapshotMatcher('z', {
-            'content': 'before',
-            'timestamp': Timestamp.fromDate(before),
-          }),
-        ])));
-    expect(
-        instance
-            .collection('messages')
-            .where('timestamp', isLessThan: now.subtract(Duration(seconds: 2)))
-            .snapshots(),
-        emits(QuerySnapshotMatcher([])));
-    expect(
-        instance
-            .collection('messages')
-            .where('timestamp', isGreaterThanOrEqualTo: now)
-            .snapshots(),
-        emits(QuerySnapshotMatcher([
-          DocumentSnapshotMatcher('zz', {
-            'content': 'during',
-            'timestamp': Timestamp.fromDate(now),
-          }),
-          DocumentSnapshotMatcher('zzz', {
-            'content': 'after',
-            'timestamp': Timestamp.fromDate(after),
-          }),
-        ])));
-    expect(
-        instance
-            .collection('messages')
-            .where('timestamp',
-                isGreaterThanOrEqualTo: now.add(Duration(seconds: 2)))
-            .snapshots(),
-        emits(QuerySnapshotMatcher([])));
   });
 
-  test('isEqualTo, orderBy, limit and getDocuments', () async {
-    final instance = MockFirestoreInstance();
-    final now = DateTime.now();
-    final bookmarks = await instance
-        .collection('users')
-        .document(uid)
-        .collection('bookmarks');
-    await bookmarks.add({
-      'hidden': false,
-      'timestamp': now,
-    });
-    await bookmarks.add({
-      'tag': 'mostrecent',
-      'hidden': false,
-      'timestamp': now.add(Duration(days: 1)),
-    });
-    await bookmarks.add({
-      'hidden': false,
-      'timestamp': now,
-    });
-    await bookmarks.add({
-      'hidden': true,
-      'timestamp': now,
-    });
-    final snapshot = (await instance
-        .collection('users')
-        .document(uid)
-        .collection('bookmarks')
-        .where('hidden', isEqualTo: false)
-        .orderBy('timestamp', descending: true)
-        .limit(2)
-        .getDocuments());
-    expect(snapshot.documents.length, equals(2));
-    expect(snapshot.documents.first['tag'], equals('mostrecent'));
-  });
-  test('Collection.getDocuments', () async {
-    final instance = MockFirestoreInstance();
-    await instance.collection('users').add({
-      'username': 'Bob',
-    });
-    final snapshot = await instance.collection('users').getDocuments();
-    expect(snapshot.documents.length, equals(1));
-  });
   test('delete', () async {
     final instance = MockFirestoreInstance();
     await instance.collection('users').document(uid).setData({
@@ -542,84 +360,4 @@ void main() {
     expect(snapshot2.documentID.length, 20);
     expect(snapshot2.exists, false);
   });
-}
-
-class QuerySnapshotMatcher implements Matcher {
-  List<DocumentSnapshotMatcher> _documentSnapshotMatchers;
-
-  QuerySnapshotMatcher(this._documentSnapshotMatchers);
-
-  @override
-  Description describe(Description description) {
-    return StringDescription("Matches a query snapshot's DocumentSnapshots.");
-  }
-
-  @override
-  Description describeMismatch(
-      item, Description mismatchDescription, Map matchState, bool verbose) {
-    mismatchDescription.add("Snapshot does not match expected data.");
-
-    // TODO: this will crash if there are fewer matchers than documents.
-
-    final snapshot = item as QuerySnapshot;
-    for (var i = 0; i < snapshot.documents.length; i++) {
-      final matcher = _documentSnapshotMatchers[i];
-      final item = snapshot.documents[i];
-      if (!matcher.matches(item, matchState)) {
-        matcher.describeMismatch(
-            item, mismatchDescription, matchState, verbose);
-      }
-    }
-    return mismatchDescription;
-  }
-
-  @override
-  bool matches(item, Map matchState) {
-    final snapshot = item as QuerySnapshot;
-    if (snapshot.documents.length != _documentSnapshotMatchers.length) {
-      return false;
-    }
-    for (var i = 0; i < snapshot.documents.length; i++) {
-      final matcher = _documentSnapshotMatchers[i];
-      if (!matcher.matches(snapshot.documents[i], matchState)) {
-        return false;
-      }
-    }
-    return true;
-  }
-}
-
-class DocumentSnapshotMatcher implements Matcher {
-  String _documentId;
-  Map<String, dynamic> _data;
-
-  DocumentSnapshotMatcher(this._documentId, this._data);
-
-  @override
-  Description describe(Description description) {
-    return StringDescription("Matches a snapshot's documentId and data");
-  }
-
-  @override
-  Description describeMismatch(
-      item, Description mismatchDescription, Map matchState, bool verbose) {
-    final snapshot = item as DocumentSnapshot;
-    // TODO: generate more meaningful descriptions.
-    if (!equals(snapshot.documentID).matches(_documentId, matchState)) {
-      equals(snapshot.documentID).describeMismatch(
-          _documentId, mismatchDescription, matchState, verbose);
-    }
-    if (!equals(snapshot.data).matches(_data, matchState)) {
-      equals(snapshot.data)
-          .describeMismatch(_data, mismatchDescription, matchState, verbose);
-    }
-    return mismatchDescription;
-  }
-
-  @override
-  bool matches(item, Map matchState) {
-    final snapshot = item as DocumentSnapshot;
-    return equals(snapshot.documentID).matches(_documentId, matchState) &&
-        equals(snapshot.data).matches(_data, matchState);
-  }
 }
---
+++ test/query_snapshot_matcher.dart
@@ -0,0 +1,49 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:test/test.dart';
+
+import 'document_snapshot_matcher.dart';
+
+class QuerySnapshotMatcher implements Matcher {
+  List<DocumentSnapshotMatcher> _documentSnapshotMatchers;
+
+  QuerySnapshotMatcher(this._documentSnapshotMatchers);
+
+  @override
+  Description describe(Description description) {
+    return StringDescription("Matches a query snapshot's DocumentSnapshots.");
+  }
+
+  @override
+  Description describeMismatch(
+      item, Description mismatchDescription, Map matchState, bool verbose) {
+    mismatchDescription.add("Snapshot does not match expected data.");
+
+    // TODO: this will crash if there are fewer matchers than documents.
+
+    final snapshot = item as QuerySnapshot;
+    for (var i = 0; i < snapshot.documents.length; i++) {
+      final matcher = _documentSnapshotMatchers[i];
+      final item = snapshot.documents[i];
+      if (!matcher.matches(item, matchState)) {
+        matcher.describeMismatch(
+            item, mismatchDescription, matchState, verbose);
+      }
+    }
+    return mismatchDescription;
+  }
+
+  @override
+  bool matches(item, Map matchState) {
+    final snapshot = item as QuerySnapshot;
+    if (snapshot.documents.length != _documentSnapshotMatchers.length) {
+      return false;
+    }
+    for (var i = 0; i < snapshot.documents.length; i++) {
+      final matcher = _documentSnapshotMatchers[i];
+      if (!matcher.matches(snapshot.documents[i], matchState)) {
+        return false;
+      }
+    }
+    return true;
+  }
+}
