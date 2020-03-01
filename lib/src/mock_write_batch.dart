b812e3bfc3d067ca40cac5d0a9ba6db53c2f17e3
suztomo <suztomo@gmail.com>
2020-03-01 10:08:42 -0500

    Batch writes updateData and delete

--- lib/src/mock_write_batch.dart
+++ lib/src/mock_write_batch.dart
@@ -10,15 +10,41 @@ class MockWriteBatch extends Mock implements WriteBatch {
   void setData(DocumentReference document, Map<String, dynamic> data,
       {bool merge = false}) {
     tasks.add(WriteTask()
+      ..command = WriteCommand.setData
       ..document = document
       ..data = data
       ..merge = merge);
   }
 
   @override
+  void updateData(DocumentReference document, Map<String, dynamic> data) {
+    tasks.add(WriteTask()
+      ..command = WriteCommand.updateData
+      ..document = document
+      ..data = data);
+  }
+
+  @override
+  void delete(DocumentReference document) {
+    tasks.add(WriteTask()
+      ..command = WriteCommand.delete
+      ..document = document);
+  }
+
+  @override
   Future<void> commit() {
     for (final task in tasks) {
-      task.document.setData(task.data, merge: task.merge);
+      switch (task.command) {
+        case WriteCommand.setData:
+          task.document.setData(task.data, merge: task.merge);
+          break;
+        case WriteCommand.updateData:
+          task.document.updateData(task.data);
+          break;
+        case WriteCommand.delete:
+          task.document.delete();
+          break;
+      }
     }
     tasks.clear();
     return Future.value();
