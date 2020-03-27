db955e1a70fd8af85e76acc945fe18976cac49d8
suztomo <suztomo@gmail.com>
2020-03-27 09:06:06 -0400

    format

--- lib/src/mock_field_value_factory_platform.dart
+++ lib/src/mock_field_value_factory_platform.dart
@@ -2,7 +2,8 @@ import 'package:cloud_firestore_mocks/src/mock_field_value_platform.dart';
 import 'package:cloud_firestore_platform_interface/cloud_firestore_platform_interface.dart';
 import 'package:plugin_platform_interface/plugin_platform_interface.dart';
 
-class MockFieldValueFactoryPlatform with
+class MockFieldValueFactoryPlatform
+    with
         // ignore: invalid_use_of_visible_for_testing_member
         MockPlatformInterfaceMixin
     implements
