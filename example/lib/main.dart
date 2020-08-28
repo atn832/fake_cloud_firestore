8a1861d4b3327b620677594cb3d75f523d1b837e
ShimizuNaoki <sensuikan1973@gmail.com>
2020-08-28 23:20:55 +0900

    enableFlutterDriverExtension. See: https://api.flutter.dev/flutter/flutter_driver_extension/flutter_driver_extension-library.html

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -7,8 +7,10 @@ import 'dart:async';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_driver/driver_extension.dart';
 
 Future<void> main() async {
+  enableFlutterDriverExtension();
   WidgetsFlutterBinding.ensureInitialized();
   final app = await Firebase.initializeApp(
     name: 'test',
