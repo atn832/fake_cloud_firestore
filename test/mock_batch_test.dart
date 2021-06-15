efedb20aa1cf87b7a2541365fbe9806c34bfbe8e
YuKiO-output <76410303+YuKiO-output@users.noreply.github.com>
2021-06-15 15:01:32 +0900

    fix Analyzer issues and Unit test issues
    fix Analyzer issues and Unit test issues

--- test/mock_batch_test.dart
+++ test/mock_batch_test.dart
@@ -1,32 +1,33 @@
 import 'dart:math';
-import 'package:cloud_firestore_mocks/cloud_firestore_mocks.dart';
+import 'package:fake_cloud_firestore/fake_cloud_firestore.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() async {
-  final usersPath = "users";
-  final firestore = MockFirestoreInstance();
+  final usersPath = 'users';
+
   Map<String, dynamic> userDocumentMock(String userId) {
     return {
-      "id": userId,
-      "updatedAt": DateTime.now(),
-      "createdAt": DateTime.now(),
+      'id': userId,
+      'updatedAt': DateTime.now(),
+      'createdAt': DateTime.now(),
     };
   }
 
   Future<List<Map<String, dynamic>>> createUsers({required int times}) async {
-    List<Map<String, dynamic>> userDataList = [];
-    for (int i = 0; i < times; i++) {
+    final userDataList = [];
+    for (var i = 0; i < times; i++) {
       final userId = i.toString();
       userDataList.add(userDocumentMock(userId));
     }
     return userDataList;
   }
 
-  group("batch", () {
-    test("succees when create 500 documents at once.", () async {
+  group('batch', () {
+    test('succees when create 500 documents at once.', () async {
+      final firestore = FakeFirebaseFirestore();
       final userDataList = await createUsers(times: 500);
       final userDataListLength = userDataList.length;
-      for (int i = 0; i < userDataListLength; i = i + 500) {
+      for (var i = 0; i < userDataListLength; i = i + 500) {
         final first = i;
         final last = min(i + 500, userDataListLength);
 
@@ -34,7 +35,7 @@ void main() async {
         final batch = firestore.batch();
         for (final userData in selectUserDataList) {
           final documentRef =
-              firestore.collection(usersPath).doc(userData["id"]);
+              firestore.collection(usersPath).doc(userData['id']);
           batch.set(documentRef, userData);
         }
         await batch.commit();
@@ -43,11 +44,12 @@ void main() async {
       expect(result.docs.length, 500);
     });
 
-    test("fail when create 501 documents at once.", () async {
+    test('fail when create 501 documents at once.', () async {
+      final firestore = FakeFirebaseFirestore();
       final userDataList = await createUsers(times: 501);
       final batch = firestore.batch();
       for (final userData in userDataList) {
-        final documentRef = firestore.collection(usersPath).doc(userData["id"]);
+        final documentRef = firestore.collection(usersPath).doc(userData['id']);
         batch.set(documentRef, userData);
       }
       expect(() => batch.commit(), throwsException);
