bf3b40f62d10263cad32a8f303239e6a92a5b55c
Sami Yildirim <49212921+Sapphirewhale@users.noreply.github.com>
2022-06-25 05:04:46 +1000

    Add test to pick up TypeError when replacing null with map
    Current code expects that if new value for a field is a Map, and the key for that field exists in the doc, then the value is already a map. This is not always the case, as demonstrated by this test.

--- test/fake_cloud_firestore_test.dart
+++ test/fake_cloud_firestore_test.dart
@@ -1354,6 +1354,24 @@ void main() {
         expect(snapshot.docs.first.data()!.title, equals(MovieTitle));
       }));
     });
+
+    test('Replace Null with Map', () async {
+      final firestore = FakeFirebaseFirestore();
+
+      var testMap = {'testVal': null};
+      await firestore
+          .collection('testCollection')
+          .doc('testDoc')
+          .update(testMap);
+
+      var newMap = {
+        'testVal': {'innerKey': 'innerVal'}
+      };
+      await firestore
+          .collection('testCollection')
+          .doc('testDoc')
+          .update(newMap);
+    });
   });
 }
 
