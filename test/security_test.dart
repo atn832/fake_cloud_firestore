3f08fe010430fdf04dddb66809ac6b3ef81855d9
Anh Tuan <atn832@gmail.com>
2023-01-09 22:50:15 +0900

    use the `await Future.value()` trick to wait for auth objects

--- test/security_test.dart
+++ test/security_test.dart
@@ -78,9 +78,6 @@ void main() {
 
     // Authenticated.
     auth.add({'uid': 'abc'});
-    // Wait for the Streams to have fired. Somehow, auth.add does not reflect in
-    // time in authObject.
-    await Future.delayed(Duration(seconds: 0));
 
     expect(
         () => instance.doc('users/abc').set({'name': 'zeta'}), returnsNormally);
